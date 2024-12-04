830c26088c674c9018e2908a30f3c719f6f89125
sonchasas <sssolovova@gmail.com>
2024-12-04 12:07:28 +0300

    commit

--- script.js
+++ script.js
@@ -1,73 +1,32 @@
-const sweaters = [
-    {
-      image: "001.JPG",
-      description: "Теплый свитер из шерсти",
-      price: 2500
-    },
-    {
-      image: "002.JPG",
-      description: "Стильный свитер с узором",
-      price: 3000
-    },
-    {
-      image: "003.JPG",
-      description: "Мягкий свитер из кашемира",
-      price: 4000
-    },
-    {
-      image: "004.JPG",
-      description: "Классический свитер с воротником",
-      price: 2800
-    },
-    {
-      image: "005.JPG",
-      description: "Уютный свитер с капюшоном",
-      price: 3500
-    },
-    {
-      image: "006.JPG",
-      description: "Тонкий свитер из хлопка",
-      price: 2200
-    },
-    {
-        image: "007.JPG",
-        description: "Тонкий свитер из хлопка",
-        price: 2200
-      },
-      {
-        image: "008.JPG",
-        description: "Тонкий свитер из хлопка",
-        price: 2200
-      },
-      {
-        image: "009.JPG",
-        description: "Тонкий свитер из хлопка",
-        price: 2200
-      }
-      
-  ];
-  
-  // Функция для создания блока с информацией о свитере
-  function createProductBlock(sweater) {
-    const block = document.createElement("div");
-    block.classList.add("product-block");
-  
-    const image = document.createElement("img");
-    image.src = sweater.image;
-    block.appendChild(image);
-  
-    const description = document.createElement("h3");
-    description.textContent = sweater.description;
-    block.appendChild(description);
-  
-    const price = document.createElement("p");
-    price.textContent = `Цена: ${sweater.price} руб.`;
-    block.appendChild(price);
-    return block;}
-    const productContainer = document.getElementById("product-container");
+async function getResponce() {
+    // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
+    //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
+    //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
+//    let responce = await fetch("../../tmp/g06u28.shop.json")
+    let responce = await fetch("../data/shop.json")
+    console.log("responce:\n", responce, "\n /responce: \n")
+    let content = await responce.text()
+    console.log("await responce.text()\n", content)
+    content = JSON.parse(content)
+    content = content.slice(0, 9)
+    console.log("content.slice(0, 9)", content)
+    let key
+    for (key in content) {
+        console.log(content[key].id, content[key].title)
+        console.log(content[key])
+    }
 
-sweaters.forEach(sweater => {
-  const productBlock = createProductBlock(sweater);
-  productContainer.appendChild(productBlock);
-});
-
+    let node_for_insert = document.getElementById("node_for_insert")
+    for (key in content) {
+        node_for_insert.innerHTML += `
+        <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
+        <img style="width: 180px" class="align-self-center" src=${content[key].img}>
+        <h5 class="card-title">${content[key].title}</h5>
+        <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
+        <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
+        <p class="card-text" >Заказать <input class="w-25" type="number" name="amount" value="0"></p>
+        </li>
+                `
+    }
+}
+getResponce()
